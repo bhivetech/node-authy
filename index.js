310b5a6d13224d53f7d5bd6e5febfe6076cc4f61
Shaul Hameed <shaulhameed.it@gmail.com>
2018-02-14 14:03:36 +0400

    Adding the suport for locale

--- index.js
+++ index.js
@@ -6,9 +6,49 @@ module.exports = function (api_key, api_url) {
     return new Authy(api_key, api_url);
 };
 
+
+
+
 function Authy(apiKey, api_url) {
     this.apiKey = apiKey;
     this.apiURL = api_url || "https://api.authy.com";
+    this.Languages = {
+        Afrikaans: "af",
+        Arabic: "ar",
+        Catalan: "ca",
+        Chinese: "zh",
+        ChineseMandarin: "zh-CN",
+        ChineseCantonese: "zh-HK",
+        Croatian: "hr",
+        Czech: "cs",
+        Danish: "da",
+        Dutch: "nl",
+        English: "en",
+        Finnish: "fi",
+        French: "fr",
+        German: "de",
+        Greek: "el",
+        Hebrew: "he",
+        Hindi: "hi",
+        Hungarian: "hu",
+        Indonesian: "id",
+        Italian: "it",
+        Japanese: "ja",
+        Korean: "ko",
+        Malay: "ms",
+        Norwegian: "nb",
+        Polish: "pl",
+        PortugueseBrazil: "pt-BR",
+        Portuguese: "pt",
+        Romanian: "ro",
+        Russian: "ru",
+        Spanish: "es",
+        Swedish: "sv",
+        Tagalog: "tl",
+        Thai: "th",
+        Turkish: "tr",
+        Vietnamese: "vi"
+    }
 }
 
 Authy.prototype.register_user = function (email, cellphone, country_code, send_sms_install_link, callback) {
@@ -17,17 +57,14 @@ Authy.prototype.register_user = function (email, cellphone, country_code, send_s
     if (arguments.length > 4) {
         country = country_code;
         send_install_link = send_sms_install_link;
-    }
-    else if (arguments.length == 4) {
+    } else if (arguments.length == 4) {
         callback = send_sms_install_link;
         if (typeof country_code == "boolean") {
             send_install_link = country_code;
-        }
-        else {
+        } else {
             country = country_code;
         }
-    }
-    else {
+    } else {
         callback = country_code;
     }
 
@@ -36,9 +73,8 @@ Authy.prototype.register_user = function (email, cellphone, country_code, send_s
             "user[cellphone]": cellphone,
             "user[country_code]": country
         },
-        callback,
-        {
-          send_install_link_via_sms: send_install_link
+        callback, {
+            send_install_link_via_sms: send_install_link
         }
     );
 };
@@ -62,8 +98,8 @@ Authy.prototype.verify = function (id, token, force, callback) {
 
     cleanToken = String(token).replace(/\D/g, "").substring(0, 16)
     // Overwrite the default body to check the response.
-    check_body_callback = function(err, res) {
-        if(!err && res.token != "is valid") {
+    check_body_callback = function (err, res) {
+        if (!err && res.token != "is valid") {
             err = {
                 message: "Unknown API response."
             }
@@ -102,26 +138,30 @@ Authy.prototype.application_stats = function (callback) {
     this._request("get", "/protected/json/app/stats", {}, callback, {});
 };
 
-Authy.prototype.phones = function() {
+Authy.prototype.phones = function () {
     self = this;
     return {
-        verification_start: function(phone_number, country_code, via, callback, code_length) {
-            if(arguments.length == 3) {
+        verification_start: function (phone_number, country_code, via, language, callback, code_length) {
+            if (arguments.length == 3) {
                 callback = via;
                 via = "sms";
             }
             options = {
                 phone_number: phone_number,
                 country_code: country_code,
-                via: via
+                via: via,
+
             };
-            if(!!code_length){
+            if (language) {
+                options.locale = language
+            }
+            if (!!code_length) {
                 options.code_length = code_length
             }
             self._request("post", "/protected/json/phones/verification/start", options, callback);
         },
 
-        verification_check: function(phone_number, country_code, verification_code, callback) {
+        verification_check: function (phone_number, country_code, verification_code, callback) {
             options = {
                 phone_number: phone_number,
                 country_code: country_code,
@@ -130,7 +170,7 @@ Authy.prototype.phones = function() {
             self._request("get", "/protected/json/phones/verification/check", options, callback);
         },
 
-        info: function(phone_number, country_code, callback) {
+        info: function (phone_number, country_code, callback) {
             options = {
                 phone_number: phone_number,
                 country_code: country_code
@@ -140,11 +180,11 @@ Authy.prototype.phones = function() {
     };
 };
 
-Authy.prototype._request = function(type, path, params, callback, qs) {
+Authy.prototype._request = function (type, path, params, callback, qs) {
     qs = qs || {}
     qs['api_key'] = this.apiKey;
 
-    user_agent = "AuthyNode/"+VERSION+" (node "+process.version+")"
+    user_agent = "AuthyNode/" + VERSION + " (node " + process.version + ")"
     headers = {
         "User-Agent": user_agent
     }
@@ -161,7 +201,7 @@ Authy.prototype._request = function(type, path, params, callback, qs) {
 
     var callback_check = function (err, res, body) {
         if (!err) {
-            if(res.statusCode === 200) {
+            if (res.statusCode === 200) {
                 callback(null, body);
             } else {
                 callback(body);
@@ -171,7 +211,7 @@ Authy.prototype._request = function(type, path, params, callback, qs) {
         }
     };
 
-    switch(type) {
+    switch (type) {
 
         case "post":
             request.post(options, callback_check);
